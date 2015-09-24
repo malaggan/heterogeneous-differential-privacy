9421a89fa715b3ec25cab92846fb5b000859eb8c
Mohammad Alaggan <mohammad.nabil.h@gmail.com>
2015-09-24 14:41:46 -0500

    Moved hash method inside abstract_user

--- abstract_user.cc
+++
@@ -1,6 +0,0 @@
-#include "abstract_user.hh"
-
-bool std::equal_to<abstract_user*>::operator()(const abstract_user* a, const abstract_user* b) const
-{
-	return a->id == b->id;
-}
--- abstract_user.hh
+++ abstract_user.hh
@@ -6,16 +6,7 @@
 
 class abstract_user;
 
-// set_t key equality test
-namespace std {
-	template <> struct equal_to<abstract_user*>
-	{
-		bool operator()(const abstract_user* a, const abstract_user* b) const;
-	};
-}
-
 #include "view_t.hh"
-#include "set_t.hh"
 
 using all_t     = std::unordered_map<user_id_t, abstract_user*>;
 
@@ -23,9 +14,25 @@ class abstract_user {
 public:
 	user_id_t id;
 public:
+	struct hash {
+		std::size_t operator()(abstract_user const *u) const {
+			return std::hash<user_id_t>()(u->id);
+		}
+	};
+
+	// --- Key equality (for unordered_set)
+	struct key_eq {
+		bool operator()(abstract_user const *a, abstract_user const *b) const {
+			return a->id == b->id;
+		}
+	};
+
+
 	explicit abstract_user(user_id_t id) : id{id} {}
 
 	virtual void do_gossip() = 0; // TODO : remember to user "override" in subclasses. TODO convert to coroutine
 	virtual void print_view() const {}
 	virtual ~abstract_user() {}
 };
+
+#include "set_t.hh"
