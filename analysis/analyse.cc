e55a3b77a18ec366bd1c4016ccf373cc86d918d8
Mohammad Alaggan <mohammad.nabil.h@gmail.com>
2015-12-02 04:26:58 +0200

    Refactoring.
    Signed-off-by: Mohammad Alaggan <mohammad.nabil.h@gmail.com>

--- analysis/analyse.cc
+++ analysis/analyse.cc
@@ -66,10 +66,52 @@ void output_slices_plot(std::unordered_map<std::tuple<dataset_t, uint32_t, min_t
 
 }
 
+void output_min_plot(std::unordered_map<std::tuple<dataset_t, min_t>, acc> const &min_expr_values) {
+	using namespace std;
+	cout << R"(\begin{figure})" << endl
+	     << R"(  \centering)" << endl
+	     << R"(  \begin{tikzpicture})" << endl
+	     << R"(    \begin{groupplot}[ %)" << endl
+	     << R"(      group style={ group name=my plots0, group size=3 by 1, x descriptions at=edge bottom, horizontal sep=2cm, vertical sep=0.1cm, },%)" << endl
+	     << R"(      footnotesize, width=4.5cm, height=5cm, xlabel=$n$, ylabel=Recall, xtick={1,2,3,4,5,6,7,8,9,10}]%))" << endl;
+	for(auto & dataset : vector<dataset_t>{dataset_t::survey, dataset_t::digg, dataset_t::delicious}) {
+		cout << R"(\nextgroupplot % )" << dataset << endl;
+		for(auto & min : vector<min_t>{min_t::zero, min_t::half, min_t::nine}) {
+			cout << R"(         \addplot[mark=)";
+			switch(min) {
+			case min_t::zero : cout << "o"; break;
+			case min_t::half : cout << "square"; break;
+			case min_t::nine : cout << "diamond"; break;
+			default : assert(false);
+			}
+			cout << R"(,error bars/.cd,y dir=both, y explicit] plot coordinates { )";
+
+			auto k = make_tuple(dataset, min);
+			assert(min_expr_values.count(k));
+			auto & acc  = min_expr_values.at(k);
+			double mean, ci; tie(mean,ci) = mean_range(acc);
+			cout << "(" << min <<"," << mean << ") +- (0,{" << ci << "}) ";
+
+			cout << " };%			" << endl;
+		}
+	}
+	cout << R"(    \end{groupplot})" << endl
+	     << R"(    \draw (my plots0 c2r1.south)+(0pt,-50pt) node {\ref{groups_legend1}};)" << endl
+	     << R"(    \draw (my plots0 c1r1.north)+(0pt,+10pt) node {Survey};)" << endl
+	     << R"(    \draw (my plots0 c2r1.north)+(0pt,+10pt) node {Digg};)" << endl
+	     << R"(    \draw (my plots0 c3r1.north)+(0pt,+10pt) node {Delicious};)" << endl
+	     << R"(  \end{tikzpicture})" << endl << endl
+	     << R"(  \caption{The value reported is the average recall obtained when all peers have the same distribution over privacy weights for all items, plotted against the number of slices.})" << endl
+	     << R"(  \label{fig:het_priv_one_group})" << endl
+	     << R"(\end{figure})" << endl;
+
+}
+
 int main()
 {
 	using namespace std;
 	unordered_map<tuple<dataset_t, uint32_t, min_t>, acc> slices_expr_values;
+	unordered_map<tuple<dataset_t, min_t>, acc> min_expr_values;
 	auto comma = boost::is_any_of(",");
 	ifstream is{"/home/malaggan/gossple/slices.csv"};
 	string str;
@@ -79,10 +121,17 @@ int main()
 		// compute mean and MeanCI
 		std::vector<std::string> toks{};
 		boost::split(toks, str, comma);
-		auto & acc = slices_expr_values[make_tuple(to_dataset(toks[0]),boost::lexical_cast<uint32_t>(toks[1]),to_min(boost::lexical_cast<double>(toks[2])))];
-		acc(boost::lexical_cast<double>(toks[3]));
+		auto dataset = to_dataset(toks[0]);
+		auto slices = boost::lexical_cast<uint32_t>(toks[1]);
+		auto min = to_min(boost::lexical_cast<double>(toks[2]));
+		auto recall = boost::lexical_cast<double>(toks[3]);
+		// auto & acc1 = slices_expr_values[make_tuple(dataset,slices,min)];
+		// acc1(recall);
+		auto & acc2 = min_expr_values[make_tuple(dataset,min)];
+		acc2(recall);
 	}
-	output_slices_plot(slices_expr_values);
+	//output_slices_plot(slices_expr_values);
+	output_min_plot(min_expr_values);
 
 	return 0;
 }
