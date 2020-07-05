01ea84e8cba3c1c7f85d0f0a12620e8dc594861a
Mohammad Alaggan <mohammad.nabil.h@gmail.com>
2020-07-04 22:47:00 -0700

    Catch exception at main

--- hdp/hdp.cc
+++ hdp/hdp.cc
@@ -18,6 +18,9 @@
 #include <algorithm>
 #include <functional>
 #include <iterator>
+
+#include <cstdio>
+
 std::string HEADER{"dataset,seed,user-id,user-class,expr,slices,min,unconcerned,normal,concerned,alpha,epsilon,recall"};
 
 namespace hdp {
@@ -111,126 +114,132 @@ void print_or_ignore(std::string const &name) {
 uint32_t current_cycle = 0;
 int      main(int argc, char *argv[]) {
     // TODO create 8 processes at a time and synchronize them using an interprocess semaphor ?
-    parse_args(argc, argv);
+    try {
+        parse_args(argc, argv);
 
-    if (vm.count("output")) {
-        hdp::redirect("output", hdp::out, std::cout);
-        hdp::out_redirected = true;
-    }
-    if (vm.count("log")) {
-        hdp::redirect("log", hdp::log, std::clog);
-        hdp::log_redirected = true;
-    }
+        if (vm.count("output")) {
+            hdp::redirect("output", hdp::out, std::cout);
+            hdp::out_redirected = true;
+        }
+        if (vm.count("log")) {
+            hdp::redirect("log", hdp::log, std::clog);
+            hdp::log_redirected = true;
+        }
 
-    static logger l{"main"};
+        static logger l{"main"};
 
-    if (vm["private"].as<bool>()) {
-        assert(vm.count("epsilon") or vm["blind"].as<bool>());
-        // must have exactly one of the three
-        assert((vm["naive"].as<bool>() and !vm["groups"].as<bool>() and !vm.count("slices") and !vm["blind"].as<bool>()) or
-               (!vm["naive"].as<bool>() and vm["groups"].as<bool>() and !vm.count("slices") and !vm["blind"].as<bool>()) or
-               (!vm["naive"].as<bool>() and !vm["groups"].as<bool>() and vm.count("slices") and !vm["blind"].as<bool>()) or
-               (!vm["naive"].as<bool>() and !vm["groups"].as<bool>() and !vm.count("slices") and vm["blind"].as<bool>()));
-    } else
-        assert(!vm.count("epsilon") and !vm["naive"].as<bool>() and !vm["groups"].as<bool>() and !vm.count("slices"));
+        if (vm["private"].as<bool>()) {
+            assert(vm.count("epsilon") or vm["blind"].as<bool>());
+            // must have exactly one of the three
+            assert((vm["naive"].as<bool>() and !vm["groups"].as<bool>() and !vm.count("slices") and !vm["blind"].as<bool>()) or
+                   (!vm["naive"].as<bool>() and vm["groups"].as<bool>() and !vm.count("slices") and !vm["blind"].as<bool>()) or
+                   (!vm["naive"].as<bool>() and !vm["groups"].as<bool>() and vm.count("slices") and !vm["blind"].as<bool>()) or
+                   (!vm["naive"].as<bool>() and !vm["groups"].as<bool>() and !vm.count("slices") and vm["blind"].as<bool>()));
+        } else
+            assert(!vm.count("epsilon") and !vm["naive"].as<bool>() and !vm["groups"].as<bool>() and !vm.count("slices"));
 
-    // for reproducibility
-    uint32_t seed;
-    if (vm.count("random-seed")) {
-        seed = vm["random-seed"].as<uint32_t>();
-    } else {
-        seed = std::uniform_int_distribution<uint32_t>{}(rng);
-    }
-    rng = std::default_random_engine{seed};
+        // for reproducibility
+        uint32_t seed;
+        if (vm.count("random-seed")) {
+            seed = vm["random-seed"].as<uint32_t>();
+        } else {
+            seed = std::uniform_int_distribution<uint32_t>{}(rng);
+        }
+        rng = std::default_random_engine{seed};
 
 
-    // TODO check paper: Push-Pull Functional Reactive Programming - Conal Elliott
-    // TODO: is search (recall) done also on RPS view??
+        // TODO check paper: Push-Pull Functional Reactive Programming - Conal Elliott
+        // TODO: is search (recall) done also on RPS view??
 
-    l.log("Initializing peers");
-    if (vm.count("dataset")) {
-        auto fname = vm["dataset"].as<std::string>();
-        if (fname != "-" /*stdin*/) {
-            fs::path path{fname};
-            if (!fs::exists(path)) {
-                l.log("File \"%s\" does not exist. Cannot read dataset. Quitting...", fname.c_str());
-                exit(1);
-            }
-            hdp::in = std::ifstream{fname};
-            std::cin.rdbuf(hdp::in.rdbuf());
-            hdp::in_redirected = true;
+        l.log("Initializing peers");
+        if (vm.count("dataset")) {
+            auto fname = vm["dataset"].as<std::string>();
+            if (fname != "-" /*stdin*/) {
+                fs::path path{fname};
+                if (!fs::exists(path)) {
+                    l.log("File \"%s\" does not exist. Cannot read dataset. Quitting...", fname.c_str());
+                    exit(1);
+                }
+                hdp::in = std::ifstream{fname};
+                std::cin.rdbuf(hdp::in.rdbuf());
+                hdp::in_redirected = true;
 
-            l.log("Reading input dataset from \"%s\"", fname.c_str());
+                l.log("Reading input dataset from \"%s\"", fname.c_str());
+            } else
+                l.log("Reading input dataset from stdin");
         } else
             l.log("Reading input dataset from stdin");
-    } else
-        l.log("Reading input dataset from stdin");
-    auto joined_peers = load_dataset();
+        auto joined_peers = load_dataset();
 
-    l.log("Simulating cycles");
+        l.log("Simulating cycles");
 
-    for (auto i : boost::counting_range(0u, cycles)) {
-        (void) i;// unused
-        current_cycle++;
+        for (auto i : boost::counting_range(0u, cycles)) {
+            (void) i;// unused
+            current_cycle++;
 
-        //boost::for_each(joined_peers, std::mem_fn(&user::vicinity_do_gossip));
-        uint32_t j = 0;
-        for (auto &v : joined_peers) {
-            v->vicinity_do_gossip();
-            ++j;
-            l.progress(current_cycle, cycles, (j % 100) == 0);
+            //boost::for_each(joined_peers, std::mem_fn(&user::vicinity_do_gossip));
+            uint32_t j = 0;
+            for (auto &v : joined_peers) {
+                v->vicinity_do_gossip();
+                ++j;
+                l.progress(current_cycle, cycles, (j % 100) == 0);
+            }
         }
+        l.log("All cycles finished");
+
+        ba::accumulator_set<double, ba::features<ba::tag::sum_kahan>> acc;
+
+        if (vm["header"].as<bool>())
+            std::cout << HEADER << std::endl;
+        boost::interprocess::file_lock flock("./results.csv");
+        flock.lock();
+        for (auto a : joined_peers) {
+
+            auto recall = a->recall();
+
+            //l.log("recall(%d) = %f", a->id, recall);
+            acc(recall);
+            print_or_na<std::string>("dataset");
+            //print_or_na<uint32_t>("random-seed");
+            std::cout << seed << ',';
+            std::cout << (a->id) << ',';
+            std::cout << a->cls() << ',';
+            // randomness type, or randomness seed
+            // --- experiment type
+            print_if("naive");
+            print_if("groups");
+            print_if("blind");
+            print_or_ignore("slices");
+            print_if_not("private", "baseline");
+            print_flag("all-peers", "some-peers");
+            // --- slices info
+            print_or_na<uint32_t>("slices");
+            print_or_na<double>("min");
+            // --- groups info
+            extern double unconcerned, normal, concerned;
+            print_or_na_or_either("naive", "groups", "unconcerned", unconcerned);
+            print_or_na_or_either("naive", "groups", "normal", normal);
+            print_or_na_or_either("naive", "groups", "concerned", concerned);
+            print_or_na<double>("alpha");
+            // --- privacy options
+            extern double epsilon;
+            std::cout << epsilon << ',';
+            std::cout << recall << std::endl;
+        }
+        std::cout << std::flush;
+        if (hdp::out_redirected) {
+            hdp::out.flush();
+            hdp::out.close();
+        }
+        flock.unlock();
+
+        auto avg_recall = ba::sum_kahan(acc) / static_cast<double>(joined_peers.size());
+
+        l.log("Recall mean: %f", avg_recall);
+    } catch (const std::exception &e) {
+        fprintf(stderr, "Exception %s\n", e.what());
+        fflush(stderr);
+        return EXIT_FAILURE;
     }
-    l.log("All cycles finished");
-
-    ba::accumulator_set<double, ba::features<ba::tag::sum_kahan>> acc;
-
-    if (vm["header"].as<bool>())
-        std::cout << HEADER << std::endl;
-    boost::interprocess::file_lock flock("./results.csv");
-    flock.lock();
-    for (auto a : joined_peers) {
-
-        auto recall = a->recall();
-
-        //l.log("recall(%d) = %f", a->id, recall);
-        acc(recall);
-        print_or_na<std::string>("dataset");
-        //print_or_na<uint32_t>("random-seed");
-        std::cout << seed << ',';
-        std::cout << (a->id) << ',';
-        std::cout << a->cls() << ',';
-        // randomness type, or randomness seed
-        // --- experiment type
-        print_if("naive");
-        print_if("groups");
-        print_if("blind");
-        print_or_ignore("slices");
-        print_if_not("private", "baseline");
-        print_flag("all-peers", "some-peers");
-        // --- slices info
-        print_or_na<uint32_t>("slices");
-        print_or_na<double>("min");
-        // --- groups info
-        extern double unconcerned, normal, concerned;
-        print_or_na_or_either("naive", "groups", "unconcerned", unconcerned);
-        print_or_na_or_either("naive", "groups", "normal", normal);
-        print_or_na_or_either("naive", "groups", "concerned", concerned);
-        print_or_na<double>("alpha");
-        // --- privacy options
-        extern double epsilon;
-        std::cout << epsilon << ',';
-        std::cout << recall << std::endl;
-    }
-    std::cout << std::flush;
-    if (hdp::out_redirected) {
-        hdp::out.flush();
-        hdp::out.close();
-    }
-    flock.unlock();
-
-    auto avg_recall = ba::sum_kahan(acc) / static_cast<double>(joined_peers.size());
-
-    l.log("Recall mean: %f", avg_recall);
     return 0;
 }
--- hdp/log.cc
+++ hdp/log.cc
@@ -4,7 +4,7 @@
 #include <vector>
 #include <iostream>
 
-namespace __detail {
+namespace detail {
 std::string va_to_str(char const *fmt, va_list args) {
     va_list v;
     va_copy(v, args);
@@ -16,43 +16,41 @@ std::string va_to_str(char const *fmt, va_list args) {
 }
 
 void produce(log_type t, std::string const &str) {
-    if (t == log_type::OUT) std::cout << str;
-    else
+    if (t == log_type::OUT) {
+        std::cout << str;
+    } else {
         std::clog << str;
+    }
 }
-}// namespace __detail
+}// namespace detail
 
-logger *             logger::most_recent_run = nullptr;
-static void          logtime();
-constexpr bool const use_arabic_locale = false;
+logger *    logger::most_recent_run = nullptr;
+static void logtime();
 
 static void vprint(std::string const &component, std::string const &msg) {
     logtime();
-    if (use_arabic_locale) __detail::produce(log_type::LOG, "\xE2\x80\x8E");// LTR character
-    // TODO use ncurses, also to detect color support
 
-    if (!hdp::log_redirected) __detail::produce(log_type::LOG, "\033[37;1m[\033[32;1m");
+    if (!hdp::log_redirected) detail::produce(log_type::LOG, "\033[37;1m[\033[32;1m");
     else
-        __detail::produce(log_type::LOG, "[");
-    __detail::produce(log_type::LOG, component);
-    if (!hdp::log_redirected) __detail::produce(log_type::LOG, "\033[37;1m]\033[0m: ");
+        detail::produce(log_type::LOG, "[");
+    detail::produce(log_type::LOG, component);
+    if (!hdp::log_redirected) detail::produce(log_type::LOG, "\033[37;1m]\033[0m: ");
     else
-        __detail::produce(log_type::LOG, "]: ");
-    __detail::produce(log_type::LOG, msg);
-    if (use_arabic_locale) __detail::produce(log_type::LOG, "\xE2\x80\x8E");// LTR character
+        detail::produce(log_type::LOG, "]: ");
+    detail::produce(log_type::LOG, msg);
 }
 
 void logger::log(std::string const &msg) {
     most_recent_run = nullptr;
     vprint(component, msg);
-    __detail::produce(log_type::LOG, "\n");
+    detail::produce(log_type::LOG, "\n");
 }
 
 void logger::log(char const *fmt, ...) {
     va_list args, v;
     va_start(args, fmt);
     va_copy(v, args);
-    auto msg = __detail::va_to_str(fmt, v);
+    auto msg = detail::va_to_str(fmt, v);
     va_end(args);
 
     log(msg);
@@ -60,18 +58,18 @@ void logger::log(char const *fmt, ...) {
 
 void logger::runlog(std::string const &msg) {
     if (!hdp::log_redirected && most_recent_run && most_recent_run->component == this->component)
-        __detail::produce(log_type::LOG, "\033[1F");// move one line up
+        detail::produce(log_type::LOG, "\033[1F");// move one line up
     most_recent_run = this;
 
     vprint(component, msg);
-    __detail::produce(log_type::LOG, "\n");
+    detail::produce(log_type::LOG, "\n");
 }
 
 void logger::runlog(char const *fmt, ...) {
     va_list args, v;
     va_start(args, fmt);
     va_copy(v, args);
-    auto msg = __detail::va_to_str(fmt, v);
+    auto msg = detail::va_to_str(fmt, v);
     va_end(args);
 
     runlog(msg);
@@ -97,7 +95,8 @@ void logger::progress(uint32_t value, uint32_t max, bool advance_tick) {
     progress_bar.push_back('[');
     if (value > max)
         value = max;
-    double v = static_cast<double>(value), m = static_cast<double>(max);
+    auto v = static_cast<double>(value);
+    auto m = static_cast<double>(max);
     v /= m;// noramlize
     v *= 50;
     value = static_cast<uint32_t>(std::round(v));
@@ -120,17 +119,19 @@ void logtime() {
     std::time_t       t  = std::time(nullptr);
     std::tm           tm = *std::localtime(&t);
     std::stringstream s;
-    if (use_arabic_locale) s.imbue(std::locale("ar_EG.utf8"));
-    else
-        s.imbue(std::locale("en_US.utf8"));
-    if (use_arabic_locale) s << "\xE2\x80\x8F";// RTL mark
-    if (!hdp::log_redirected) s << "\033[37;1m[\033[35;1m";
-    else
+
+    if (!hdp::log_redirected) {
+        s << "\033[37;1m[\033[35;1m";
+    } else {
         s << "[";
+    }
+
     s << std::put_time(&tm, "%c");
-    if (!hdp::log_redirected) s << "\033[37;1m]\033[0m: ";
-    else
+    if (!hdp::log_redirected) {
+        s << "\033[37;1m]\033[0m: ";
+    } else {
         s << "]: ";
+    }
     s << ' ';// %Z for timezone.
-    __detail::produce(log_type::LOG, s.str());
+    detail::produce(log_type::LOG, s.str());
 }
